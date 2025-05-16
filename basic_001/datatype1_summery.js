06b184e1f7308dd5b92867b84e16be64bd56aa3e
nandkumar laxman birgad <nandkumarbirgad5@gmail.com>
2025-05-16 19:22:15 +0000

    data type

---
+++ basic_001/datatype1_summery.js
@@ -0,0 +1,13 @@
+const loggedId = false
+let useemail;
+const outsideTemp = null
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+
+
+let myObj = {
+    myName:"nandu",
+    age: 23,
+}
+const cars =["lamborgini","bmw","oddi","tata"]
+console.log(cars)
---
+++ basic_001/variable_01.js
@@ -0,0 +1,9 @@
+const  accountId = 12344
+let accountemailId = "nandu@GMAIL.COM"
+var acountPassword = "122213124"
+
+console.log(accountId);
+accountId = 676575
+accountemailId = "ghfh@gjhjh"
+acountPassword = "766444"
+console.table([accountId, accountemailId, acountPassword])
